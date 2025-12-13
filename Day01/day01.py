6bfac90e45ebcd4c910ea1d98d7ee62e3436c4cd
David Jay Martin <davidjay43@gmail.com>
2025-12-12 20:23:00 -0500

    added comments to document the steps to solve the problem.

--- Day01/day01.py
+++ Day01/day01.py
@@ -4,52 +4,35 @@
 dial_position = 50
 zero_counter = 0
 
-# Read lines from input file and replace L's with - and R's with nothing
-# Append the int to the dial_moves list
+# Read lines from input file and create list of dial moves [direction, turn distance]
 with open("Day01/input.txt", "r") as f:
     lines = f.readlines()
 
 moves = [(move[0], int(move[1:])) for move in lines]
 
-# iterate over the list of dial moves to update the dial position
+# iterate over the list of dial moves
 for move in moves:
     
+    # cycle through full dial rotations and count the tiems past zero
     zeros, rotation = divmod(move[1], 100)
     zero_counter += zeros
 
+    # if turning to the right and if the initial position
+    # plus the dial move is >= 100, increment counter
     if move[0] == "R":
         if dial_position + rotation >= 100:
             zero_counter += 1
+        
+        # update the dial position
         dial_position = (dial_position + rotation) % 100
+    
+    # if turing to left,  and if the initial position
+    # minus the dial move is <= 100, increment counter
     else:
         if dial_position and dial_position - rotation <= 0:
             zero_counter += 1
+        
+        # update the dial position
         dial_position = (dial_position - rotation) % 100
 
 print(zero_counter)
-
-
-
-    # # save the last position and move the dial
-    # last_position = dial_position
-    # dial_position = dial_position + move
-
-    # # check if the dial is > 100 away from zero
-    # # count the full turns of the dial
-    # # reset the dial_position to a number in the valid range
-    # if dial_position > 99 or dial_position < -99:
-    #     multiplier = divmod(dial_position, 100)[0]
-    #     zero_counter += abs(multiplier)
-    #     dial_position = dial_position - (100 * multiplier)
-
-    # # the dial stayed within 100 of zero
-    # # check if there was a sign change or if it landed on zero   
-    # else:
-    #     if dial_position == 0:
-    #         zero_counter += 1
-    #     if (dial_position > 0 and last_position < 0) or (dial_position < 0 and last_position > 0):
-    #         zero_counter += 1
-
-    #     # move the dial back to the valid range
-    #     if dial_position < 0:
-    #         dial_position = dial_position + 100
